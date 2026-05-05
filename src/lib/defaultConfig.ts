0c6dae56d4a0101f75fd1c0c455ec23a9b34f25c
shanmukha <shanmukha.p@gmail.com>
2026-05-05 15:45:40 +0530

    dd

--- src/lib/defaultConfig.ts
+++ src/lib/defaultConfig.ts
@@ -237,10 +237,10 @@ export const DEFAULT_CONFIG = {
         {
           type: 'list',
           items: [
-            'Business Name: RunOps',
+            'Business Name: RunOps Services',
             'Owner: Shanmukha P',
             'Business Type: Proprietorship',
-            'GST Number: (ADD VALID GST NUMBER)',
+            'GST Number: 29ARHPP7007J1ZE',
             'Country: India',
             'Phone: +91 9739202351',
             'Email: support@runops.in'
